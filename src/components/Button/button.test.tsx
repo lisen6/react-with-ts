5291e26163ca3c3a8429009705548f1055ba3676
lisen.6 <lisen.6@bytedance.com>
2020-11-29 18:35:07 +0800

    close husky and reset before version

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import Button, { ButtonType } from "./button";
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import Button, { ButtonType } from './button'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
-describe("test Button component", () => {
+describe('test Button component', () => {
   // test || it
-  test("should render the correct default button", () => {
-    const wrapper = render(<Button>Nice</Button>);
-    const element = wrapper.getByText("Nice");
-    expect(element.tagName).toEqual("BUTTON");
-    expect(element).toHaveClass("btn btn-default");
-  });
+  test('should render the correct default button', () => {
+    const wrapper = render(<Button>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
+  })
 
-  test("should render a link when btnType equals link and href is provided", () => {
+  test('should render a link when btnType equals link and href is provided', () => {
     const wrapper = render(
       <Button href="www.baidu.com" btnType={ButtonType.Link}>
         Nice
       </Button>
-    );
-    const element = wrapper.getByText("Nice");
-    expect(element.tagName).toEqual("A");
-    expect(element).not.toBeDisabled();
-    expect(element).toHaveAttribute("href");
-  });
-});
+    )
+    const element = wrapper.getByText('Nice')
+    expect(element.tagName).toEqual('A')
+    expect(element).not.toBeDisabled()
+    expect(element).toHaveAttribute('href')
+  })
+})
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,41 +1,41 @@
-import React, { createContext, useState, useContext } from 'react'
-import classNames from 'classnames'
+import React, { createContext, useState, useContext } from "react";
+import classNames from "classnames";
 
-type MenuMode = 'horizontal' | 'vertical'
-type SelectCallback = (selectedIndex: number) => void
+type MenuMode = "horizontal" | "vertical";
+type SelectCallback = (selectedIndex: number) => void;
 
 export interface MenuProps {
-  defaultIndex?: number
-  mode?: MenuMode
-  className?: string
-  style?: React.CSSProperties
-  onSelect?: SelectCallback
+  defaultIndex?: number;
+  mode?: MenuMode;
+  className?: string;
+  style?: React.CSSProperties;
+  onSelect?: SelectCallback;
 }
 
 interface IMenuContext {
-  index: number
-  onSelect?: SelectCallback
+  index: number;
+  onSelect?: SelectCallback;
 }
 
-export const MenuContext = createContext<IMenuContext>({ index: 0 })
+export const MenuContext = createContext<IMenuContext>({ index: 0 });
 
 const Menu: React.FC<MenuProps> = (props) => {
-  const { defaultIndex, mode, className, style, children, onSelect } = props
-  const classes = classNames('menu', className, {
-    'menu-vertical': mode === 'vertical'
-  })
-  const [currentActive, setCurrentActive] = useState(defaultIndex)
+  const { defaultIndex, mode, className, style, children, onSelect } = props;
+  const classes = classNames("menu", className, {
+    "menu-vertical": mode === "vertical",
+  });
+  const [currentActive, setCurrentActive] = useState(defaultIndex);
 
   const handleClick = (index = 0) => {
-    setCurrentActive(index)
+    setCurrentActive(index);
     if (onSelect) {
-      onSelect(index)
+      onSelect(index);
     }
-  }
+  };
   const passedContext: IMenuContext = {
     index: currentActive ? currentActive : 0,
-    onSelect: handleClick
-  }
+    onSelect: handleClick,
+  };
 
   return (
     <ul className={classes} style={style}>
@@ -43,12 +43,12 @@ const Menu: React.FC<MenuProps> = (props) => {
         {children}
       </MenuContext.Provider>
     </ul>
-  )
-}
+  );
+};
 
 Menu.defaultProps = {
   defaultIndex: 0,
-  mode: 'horizontal'
-}
+  mode: "horizontal",
+};
 
-export default Menu
+export default Menu;
