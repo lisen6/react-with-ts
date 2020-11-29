e5b0e2fb815fa3123428a4ae27e42f6eb41b2718
lisen.6 <lisen.6@bytedance.com>
2020-11-29 17:54:07 +0800

    add menu component

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,57 @@ import React from "react";
 import "./styles/index.scss";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
+
 function App() {
   return (
-    <div className="App" style={{}}>
-      <Button onClick={(e) => console.log(e, 123)} btnType={ButtonType.Default}>
-        Button
-      </Button>
-      <Button disabled btnType={ButtonType.Default}>
-        Disabled Button
-      </Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
-        Button Large
-      </Button>
-      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
-        Button Small
-      </Button>
+    <>
+      <div className="Button-wrapper">
+        <Button
+          onClick={(e) => console.log(e, 123)}
+          btnType={ButtonType.Default}
+        >
+          Button
+        </Button>
+        <Button disabled btnType={ButtonType.Default}>
+          Disabled Button
+        </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Button Large
+        </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+          Button Small
+        </Button>
 
-      <Button
-        href="/baidu.com"
-        btnType={ButtonType.Link}
-        size={ButtonSize.Small}
-      >
-        Link
-      </Button>
-      <Button
-        disabled
-        href="/baidu.com"
-        btnType={ButtonType.Link}
-        size={ButtonSize.Small}
-      >
-        Disabled Link
-      </Button>
-    </div>
+        <Button
+          href="/baidu.com"
+          btnType={ButtonType.Link}
+          size={ButtonSize.Small}
+        >
+          Link
+        </Button>
+        <Button
+          disabled
+          href="/baidu.com"
+          btnType={ButtonType.Link}
+          size={ButtonSize.Small}
+        >
+          Disabled Link
+        </Button>
+      </div>
+      <div className="Menu-wrapper">
+        <Menu
+          defaultIndex={0}
+          mode="vertical"
+          onSelect={(index) => console.log(index)}
+        >
+          <MenuItem index={0}>1</MenuItem>
+          <MenuItem index={1}>2</MenuItem>
+          <MenuItem index={2}>3</MenuItem>
+        </Menu>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Menu/menu.tsx
@@ -0,0 +1,54 @@
+import React, { createContext, useState, useContext } from 'react'
+import classNames from 'classnames'
+
+type MenuMode = 'horizontal' | 'vertical'
+type SelectCallback = (selectedIndex: number) => void
+
+export interface MenuProps {
+  defaultIndex?: number
+  mode?: MenuMode
+  className?: string
+  style?: React.CSSProperties
+  onSelect?: SelectCallback
+}
+
+interface IMenuContext {
+  index: number
+  onSelect?: SelectCallback
+}
+
+export const MenuContext = createContext<IMenuContext>({ index: 0 })
+
+const Menu: React.FC<MenuProps> = (props) => {
+  const { defaultIndex, mode, className, style, children, onSelect } = props
+  const classes = classNames('menu', className, {
+    'menu-vertical': mode === 'vertical'
+  })
+  const [currentActive, setCurrentActive] = useState(defaultIndex)
+
+  const handleClick = (index = 0) => {
+    setCurrentActive(index)
+    if (onSelect) {
+      onSelect(index)
+    }
+  }
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : 0,
+    onSelect: handleClick
+  }
+
+  return (
+    <ul className={classes} style={style}>
+      <MenuContext.Provider value={passedContext}>
+        {children}
+      </MenuContext.Provider>
+    </ul>
+  )
+}
+
+Menu.defaultProps = {
+  defaultIndex: 0,
+  mode: 'horizontal'
+}
+
+export default Menu
